586d485d7c9e3d5d00dfcf6be8ce13c810b21316
carlos <1211604@isep.ipp.pt>
2024-01-06 23:23:59 +0000

    logs

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
 # fedora packages
-sudo ./fedora-packages.sh
+#sudo ./fedora-packages.sh
 
 # install apps
-sudo ./flatpack-app-install.sh
+#sudo ./flatpack-app-install.sh
 
 # gnome extensions
-sudo ./extension-installer.sh
+#sudo ./extension-installer.sh
 
 # lamp
-sudo ./lamp.sh
+#sudo ./lamp.sh
 
 # docker
-sudo ./docker-install.sh
+#sudo ./docker-install.sh
+
+
+log_file="log.txt"
+
+echo "Install status:"
+awk '/\[SUCCESS\]/ {gsub(/.*\[SUCCESS\] /, "\033[32m&\033[0m"); print}' "$log_file"
+awk '/\[FAILURE\]/ {gsub(/.*\[FAILURE\] /, "\033[31m&\033[0m"); print}' "$log_file"
