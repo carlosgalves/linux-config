92989fe99bb9b62eae073500976855d7f1102f21
carlos <1211604@isep.ipp.pt>
2024-01-07 00:38:20 +0000

    colored progress bar

--- apps/flatpak-app-install.sh
+++ apps/flatpak-app-install.sh
@@ -55,7 +55,7 @@ show_progress() {
     local current=$1
     local total=$2
     local progress_percent=$((current * 100 / total))
-    printf "\rProgress %d/%d [%-20s] %d%%\n" "$current" "$total" $(printf '#%.0s' $(seq 1 $((progress_percent / 5)))) "$progress_percent"
+    printf "\r\033[34mProgress %d/%d [\033[36m%-20s\033[34m] %d%%\033[0m\n" "$current" "$total" $(printf '#%.0s' $(seq 1 $((progress_percent / 5)))) "$progress_percent"
 }
 
 
