6a42ddca7100b1d51bc68edf2078b0a1e5a4439a
carlos <cgalves@invisiblecloud.pt>
2024-01-25 23:20:27 +0000

    extensões

---
+++ nxt/gnome/extension-installer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# extension manager
+flatpak install flathub com.mattjakeman.ExtensionManager -y
+# gnome-extensions
+sudo dnf -y install gnome-extensions-app
+#gnome-tweaks
+sudo dnf -y install gnome-tweaks
+# adwaita-dark
+sudo dnf -y install gnome-themes-extra
+
+
+sudo dnf update -y
+
+sudo dnf install -y curl wget jq
+
+rm -f ./install-gnome-extensions.sh; wget -N -q "https://raw.githubusercontent.com/ToasterUwU/install-gnome-extensions/master/install-gnome-extensions.sh" -O ./install-gnome-extensions.sh && chmod +x install-gnome-extensions.sh
+
+sudo chmod +x gnome/extensions/extensions.txt
+./install-gnome-extensions.sh --enable --file gnome/extensions/extensions.txt
+
+sudo rm install-gnome-extensions.sh
