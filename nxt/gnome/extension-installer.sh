d056ac00f5e13a5808f95e676f3abcb415f70523
carlos <cgalves@invisiblecloud.pt>
2024-01-26 15:59:23 +0000

    extensões

--- nxt/gnome/extension-installer.sh
+++ nxt/gnome/extension-installer.sh
@@ -17,7 +17,7 @@ sudo dnf install -y curl wget jq
 
 rm -f ./install-gnome-extensions.sh; wget -N -q "https://raw.githubusercontent.com/ToasterUwU/install-gnome-extensions/master/install-gnome-extensions.sh" -O ./install-gnome-extensions.sh && chmod +x install-gnome-extensions.sh
 
-sudo chmod +x gnome/extensions.txt
-./install-gnome-extensions.sh --enable --file gnome/extensions.txt
+sudo chmod +x extensions.txt
+./install-gnome-extensions.sh --enable --file extensions.txt
 
 #sudo rm install-gnome-extensions.sh
