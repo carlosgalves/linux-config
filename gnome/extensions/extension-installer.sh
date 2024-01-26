6cc83b2bc262b13013a5ec79914565f85fe94fb3
carlos <cgalves@invisiblecloud.pt>
2024-01-26 00:54:58 +0000

    extensões

--- gnome/extensions/extension-installer.sh
+++ gnome/extensions/extension-installer.sh
@@ -12,11 +12,12 @@ sudo dnf -y install gnome-themes-extra
 
 sudo dnf update -y
 
+#dependências
 sudo dnf install -y curl wget jq
 
 rm -f ./install-gnome-extensions.sh; wget -N -q "https://raw.githubusercontent.com/ToasterUwU/install-gnome-extensions/master/install-gnome-extensions.sh" -O ./install-gnome-extensions.sh && chmod +x install-gnome-extensions.sh
 
-sudo chmod +x gnome/extensions/extensions.txt
-./install-gnome-extensions.sh --enable --file gnome/extensions/extensions.txt
+sudo chmod +x gnome/extensions.txt
+./install-gnome-extensions.sh --enable --file gnome/extensions.txt
 
 sudo rm install-gnome-extensions.sh
