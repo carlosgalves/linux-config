724dba06e66ee3f5156394025c95f8a58d55935c
carlos <cgalves@invisiblecloud.pt>
2024-01-30 21:49:43 +0000

    postman fix

--- nxt/apps/postman-fix.sh
+++ nxt/apps/postman-fix.sh
@@ -2,6 +2,7 @@
 
 dnf -y install openssl
 
+mkdir -p ~/.var/app/com.getpostman.Postman/config/Postman/proxy && \
 openssl req -subj '/C=US/CN=Postman Proxy' \
     -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 \
     -keyout ~/.var/app/com.getpostman.Postman/config/Postman/proxy/postman-proxy-ca.key \
