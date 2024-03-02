9679525d958b67dddf2394e04a5d05a36261d55d
carlos <cgalves@invisiblecloud.pt>
2024-03-02 00:00:49 +0000

    appearance

--- nxt/fedora/config.sh
+++ nxt/fedora/config.sh
@@ -4,3 +4,11 @@
 hostnamectl set-hostname --static carlos-ideapad-pro-5
 hostnamectl set-hostname --pretty "Carlos Ideapad Pro 5"
 
+# theme & appearance
+gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+
+# touchpad
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true  
